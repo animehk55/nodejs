1fddc2b48ab08bafdde73eca90af55c0a8848f58
SOF <animeshk55@gmail.com>
2019-01-28 19:32:27 +0530

    Added basics Javascript codes and notes too

---
+++ play.js
@@ -0,0 +1,46 @@
+let name = 'Max';
+const age = 29;
+let hasHobbies = true;
+
+
+name = 'shyam';
+// age = 45;       // Assignment to constant variable.
+
+// function summarizeUseer (userName, userAge, userHasHobby) {
+//   return (
+//     'Student name is ' +
+//     userName +
+//     'his age as mentioned BOD is ' +
+//     userAge +
+//     '. And his hobies as described will be as follows ' +
+//     userHasHobby
+//   );
+// }
+
+
+// Another way of writing function : named function 
+// const SummariseNNNNNNEW = function (userName, userAge, userHasHobby) {
+//     return (
+//       'Student name is ' +
+//       userName +
+//       'his age as mentioned BOD is ' +
+//       userAge +
+//       '. And his hobies as described will be as follows ' +
+//       userHasHobby
+//     );
+//   }
+  
+// Arrow function 
+const SummariseNNNNNNEW = (userName, userAge, userHasHobby)  => {
+    return (
+      'Student name is ' +
+      userName +
+      'his age as mentioned BOD is ' +
+      userAge +
+      '. And his hobies as described will be as follows ' +
+      userHasHobby
+    );
+  }
+  
+
+console.log(SummariseNNNNNNEW(name, age, hasHobbies));
