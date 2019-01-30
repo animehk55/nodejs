09b45c199e5254269214f4a4c1342ac0268928ed
SOF <animeshk55@gmail.com>
2019-01-30 15:14:56 +0530

    Resollved the code by copy pasting the code : But both are codes are same but why is this issue Comming out???? Lt me know

--- expressWithRoutes/routes/shop.js
+++ expressWithRoutes/routes/shop.js
@@ -1,13 +1,30 @@
+// const path = require('path');
+
+// const express = require('express');
+
+// const rootDir = request('../util/path');
+
+// const router = express.Router();
+
+// router.get('/', (req, res, next) => {
+//     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+// });
+
+// module.exports = router;
+
+
+// -------------------Above code is not working but below is working -------both are same code ???????
+
 const path = require('path');
 
 const express = require('express');
 
-const rootDir = request('../util/path');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
