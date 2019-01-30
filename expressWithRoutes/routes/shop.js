6f3a74cf4591354e0fbed13d2d257bf301fc2241
SOF <animeshk55@gmail.com>
2019-01-30 15:03:05 +0530

    Added ExpressCompletedCOPIED folder to check my project folder issue, unable to find my issue even if i chcked it thoroughly.

--- expressWithRoutes/routes/shop.js
+++ expressWithRoutes/routes/shop.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
 const express = require('express');
+
+const rootDir = request('../util/path');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express, Shop page</h1>')
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
